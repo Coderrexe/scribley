4faf8ccfdf3117b211b43933ee49f0260e359aa3
Coderrexe <shizimo@gmail.com>
2025-07-27 22:38:54 +0800

    implement header

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import { ClerkLoaded } from "@clerk/nextjs";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    // ClerkLoaded ensures Clerk object has loaded, so we can use Clerk
+    // functionalities like SignIn without needing to check Clerk exists.
+    <ClerkLoaded>
+      <div className="flex-1 flex flex-col h-screen">
+        <Header />
+
+        <main className="flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </ClerkLoaded>
+  );
+}
+export default DashboardLayout;
