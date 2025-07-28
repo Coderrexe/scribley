ded3646d8cf67eaae8baa52005b74b3950574dd9
Coderrexe <shizimo@gmail.com>
2025-07-28 14:04:06 +0800

    clean up clerk middleware code

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,9 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     // Always run for API routes
-//     "/(api|trpc)(.*)",
-//   ],
-// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// myapp.com/dashboard/*
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
